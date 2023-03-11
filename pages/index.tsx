cd8a564a9c83f5a56a2d724e2b221af274b2f9fe
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-11 10:47:46 -0300

    feat: create login form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,45 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    alert(form.email + " " + form.password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="text"
+          value={form.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={form.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
